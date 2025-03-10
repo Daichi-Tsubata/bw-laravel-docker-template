d79cff34145cf62e834c67c2149c8cba9d6d6a6a
Daichi-Tsubata <sooma.ryuto@gmail.com>
2025-03-11 00:19:32 +0900

    Migration、Seederファイルの作成、編集、実行

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -1,3 +1,5 @@
+src/database/seeds/DatabaseSeeder.php
+
 <?php
 
 use Illuminate\Database\Seeder;
@@ -5,12 +7,14 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            TodoSeeder::class,
+        ]);
     }
 }
